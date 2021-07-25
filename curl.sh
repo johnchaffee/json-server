31a67aaededeea04198a5b8edb2a0f53f28266ea
John Chaffee <johnchaffee@mac.com>
2021-07-24 19:35:04 -0700

    curl and json samples

--- curl.sh
+++ curl.sh
@@ -1,3 +1,6 @@
 curl http://localhost:4000/contacts
-curl --header "Content-Type: application/json" --request POST --data '{"number": "2063996576", "first": "John", "last": "Chaffee"}' http://localhost:4000/contacts
 curl http://localhost:4000/contacts/2063996576
+curl --header "Content-Type: application/json" --request POST \
+--data '{"number": "2063996576", "campaign_id": 101, "created_at": "2020-12-14T19:37:14Z", "updated_at": "2020-12-14T19:37:14Z", "zo_progress_at": "2020-12-14T19:37:16Z", "mo_received_at": "2020-12-14T19:36:18Z", "status_code": 4, "delivery_seconds": 5, "response_seconds": 10, "mo_stop": false }' \
+http://localhost:4000/contacts
+
